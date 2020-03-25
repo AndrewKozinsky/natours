b5bab6299b377331d002e089756cac15189444f1
Andrew Kozinsky <andkozinskiy@yandex.ru>
2020-03-25 11:02:47 +0500

    I have done a little refactoring

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res
-        .status(200)
-        .json({
-            status: 'success',
-            results: tours.length,
-            data: {
-                tours
-            }
-        })
-});
+    .status(200)
+    .json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+};
 
-
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
 
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
@@ -38,9 +37,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
         status: 'success',
         data: { tour }
     })
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
 
@@ -48,15 +47,15 @@ app.post('/api/v1/tours', (req, res) => {
 
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res
-            .status(201)
-            .json({
-                status: 'success',
-                data: {tour: newTour}
-            })
+        .status(201)
+        .json({
+            status: 'success',
+            data: {tour: newTour}
+        })
     });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
     if(req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'fail',
@@ -70,9 +69,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: '<Updated tour here...'
         }
     })
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
     if(req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'fail',
@@ -84,7 +83,22 @@ app.delete('/api/v1/tours/:id', (req, res) => {
         status: 'success',
         data: null
     })
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 
 const port = 3000;
